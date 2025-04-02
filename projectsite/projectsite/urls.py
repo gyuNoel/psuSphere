9453a560e695cd6f3a987cf65a7b14ae7b9a163e
breina <breina.adenig123@gmail.com>
2025-04-03 00:18:14 +0800

    updated the urls.py

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,28 +16,37 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, StudentList, StudentCreateView, StudentUpdateView,StudentDeleteView,OrgmemberList, OrgmemberCreateView,OrgmemberUpdateView, OrgmemberDeleteView,CollegeList, CollegeCreateView, CollegeUpdateView,CollegeDeleteView,ProgramList,ProgramCreateView, ProgramUpdateView,ProgramDeleteView
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
+from studentorg.views import StudentList, StudentCreateView, StudentUpdateView,StudentDeleteView
+from studentorg.views import OrgmemberList, OrgmemberCreateView,OrgmemberUpdateView, OrgmemberDeleteView
+from studentorg.views import CollegeList, CollegeCreateView, CollegeUpdateView,CollegeDeleteView
+from studentorg.views import ProgramList,ProgramCreateView, ProgramUpdateView,ProgramDeleteView
 from studentorg import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', views.HomePageView.as_view(), name='home'),
+    #Organization
     path ('organization_list',OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
     path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+    #Student
     path ('student_list',StudentList.as_view(), name='student-list'),
     path('student_list/add', StudentCreateView.as_view(), name='student-add'),
     path('student_list/<pk>', StudentUpdateView.as_view(), name='student-update'),
     path('student_list/<pk>/delete', StudentDeleteView.as_view(), name='student-delete'),
+    #Orgmember
     path ('orgmember_list',OrgmemberList.as_view(), name='orgmember-list'),
     path('orgmember_list/add', OrgmemberCreateView.as_view(), name='orgmember-add'),
     path('orgmember_list/<pk>', OrgmemberUpdateView.as_view(), name='orgmember-update'),
     path('orgmember_list/<pk>/delete', OrgmemberDeleteView.as_view(), name='orgmember-delete'),
+    #College
     path ('college_list',CollegeList.as_view(), name='college-list'),
     path('college_list/add', CollegeCreateView.as_view(), name='college-add'),
     path('college_list/<pk>',CollegeUpdateView.as_view(), name='college-update'),
     path('college_list/<pk>/delete', CollegeDeleteView.as_view(), name='college-delete'),
+    #Program
     path ('program_list',ProgramList.as_view(), name='program-list'),
     path('program_list/add', ProgramCreateView.as_view(), name='program-add'),
     path('program_list/<pk>', ProgramUpdateView.as_view(), name='program-update'),
