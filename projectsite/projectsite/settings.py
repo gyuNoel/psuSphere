17256b3eee38a32e9f4fdd328cba7a31c2a63ab8
Noel Pactanac <202280151@psu.palawan.edu.ph>
2025-04-03 14:31:16 +0800

    fix allowed hosts

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i3is!)m_jtwnbs33j$p4q%z#3v%o8cxytjo&6g#n2%hsnwoq#x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hachuling.pythonanywhere.com ','strawbrei.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['hachuling.pythonanywhere.com','strawbrei.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
